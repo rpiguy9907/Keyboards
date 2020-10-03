2a7ff8d0f9ea28a5c9a83ac21a3e73f0c1a75ade
Dustin Morgan <morgan@aspendenver.org>
2020-10-03 01:28:49 +0000

    hex

--- Southpaw66/qmk/keymaps/default/keymap.c
+++ Southpaw66/qmk/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_END,  KC_RCTL, _______,  KC_CAPS, KC_O,    _______, KC_EQL,
+        KC_END,  KC_RCTL, _______,  KC_CAPS, KC_O,    _______, _______,
         KC_RBRC, KC_BSLS, KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,
         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL         ),
 
